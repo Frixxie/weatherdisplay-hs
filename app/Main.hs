804c7b2362b7bca235ef0b41a11515224ab082d5
Fredrik <freddahf@outlook.com>
2024-08-11 11:27:23 +0200

    add cmdargs

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,14 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 
 module Main where
 
 import Data.Aeson
-import Data.ByteString.Lazy.Char8 as C8
 import Data.Time
 import GHC.Generics
 import Network.HTTP.Client
+import System.Console.CmdArgs
 
 data Coords = Coords
   { lon :: Float,
@@ -41,21 +42,33 @@ instance ToJSON Nowcast
 
 instance FromJSON Nowcast
 
+data CommandOpts = Opts
+  { url :: String,
+    loc :: String
+  }
+  deriving (Show, Data, Typeable)
+
+options :: CommandOpts
+options = Opts {url = "http://localhost:3000" &= help "Url to use" &= opt "http://localhost:3000", loc = "Heimdal" &= help "Location to lookup" &= opt "Heimdal"} &= summary "Display weather from Wictk"
+
 firstNowcast :: [Nowcast] -> Nowcast
 firstNowcast = Prelude.head
 
+printTemp :: Nowcast -> IO ()
+printTemp (Nowcast simpleEntry) = print $ temperature simpleEntry
+
 main :: IO ()
 main = do
+  lopts <- cmdArgs options
   manager <- newManager defaultManagerSettings
 
-  request <- parseRequest "http://localhost:3000/api/nowcasts?location=Heimdal"
+  let lurl = url lopts ++ "/api/nowcasts?location=" ++ loc lopts
+
+  request <- parseRequest lurl
   response <- httpLbs request manager
 
   let res = responseBody response
-
-  print res
-
-  let nowcasts = decode res :: Maybe [Nowcast]
-  case nowcasts of
-    Just nowcasts -> print $ firstNowcast nowcasts
+  let nowcastsDecoded = decode res :: Maybe [Nowcast]
+  case nowcastsDecoded of
+    Just nowcasts -> printTemp $ firstNowcast nowcasts
     Nothing -> print "Failed to decode response"
